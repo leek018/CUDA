05a9302df48691bf1ec012f5cf58ecfbdb0fae23
leek <leek018@naver.com>
2020-02-03 16:22:57 +0000

    hello cuda

---
+++ 0_helloCuda/hello.cu
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include <stdio.h>
+
+__global__ void print_kernel() {
+    printf("Hello from block %d, thread %d\n", blockIdx.x, threadIdx.x);
+}
+
+int main() {
+    print_kernel<<<10, 10>>>();
+    cudaDeviceSynchronize();
+    return 0;
+}   
